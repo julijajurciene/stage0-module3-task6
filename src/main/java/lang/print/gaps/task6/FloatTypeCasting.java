424df317cf738bff7aea49293bd17dbe2e9675f3
Julija <julija.jurciene@gmail.com>
2022-07-02 21:18:10 +0300

    commit

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        float result = Math.round(numberToBeRounded);
+        System.out.println(result);
     }
 }
--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -2,5 +2,14 @@ package lang.print.gaps.task6;
 
 public class IntegerTypeCasting {
     public static void main(String[] args) {
+
+        long first = 1000L;
+        int second = 250;
+        first = second;
+
+        System.out.println(first);
+        System.out.println(second);
+
+
     }
 }
